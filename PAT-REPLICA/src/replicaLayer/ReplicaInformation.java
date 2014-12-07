f46ef2b3af087bad0cf7740dab6402673879ff68
Paterson Deshommes <pat14_14@hotmail.com>
2014-12-07 11:10:08 -0500

    Specify in ReplicaInformation class how are components grouped in different host

--- PAT-REPLICA/src/replicaLayer/ReplicaInformation.java
+++ PAT-REPLICA/src/replicaLayer/ReplicaInformation.java
@@ -24,14 +24,14 @@ public class ReplicaInformation {
 		networkInfoList.put("replica1", "7777-132.205.95.21");
 		networkInfoList.put("replica2", "7778-127.0.0.1");
 		networkInfoList.put("replica3", "7779-127.0.0.1");
-		sequencerPort = 7780;
+		//sequencerPort = 7780;
 		frontEndPort = 2021;
 		replicaManagerPort = 7782;
 		heartbeatListenerPort = 7783;
-		sequencerIp = "127.0.0.1";
-		frontEndIp = "132.205.95.22";
-		replicaManagerIp = "127.0.0.1";
-		heartbeatListenerIp = "132.205.95.21";  //should be the same as replica ip
+		//sequencerIp = "127.0.0.1"; //should be the same as replica3 ip
+		frontEndIp = "132.205.95.22"; //should be the same as replica2 ip
+		replicaManagerIp = "127.0.0.1"; //should be the same as replica1 ip
+		heartbeatListenerIp = "132.205.95.21";  //should be the same as your replica ip
 		multicastPort = 5000;
 		multicastAddr = "224.24.24.24";
 	}
