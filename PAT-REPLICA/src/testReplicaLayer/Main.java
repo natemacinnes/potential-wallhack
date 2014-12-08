d6fca67f24f5e6ec3421cba68b53f224c59deab3
n_macinn <n_macinn@MEDAWAR.ENCS.concordia.ca>
2014-12-08 14:11:08 -0500

    Replaced Main in replicaLayer in PAT-REPLICA

---
+++ PAT-REPLICA/src/testReplicaLayer/Main.java
@@ -0,0 +1,17 @@
+package testReplicaLayer;
+
+import replicaLayer.ReplicaServer;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String replicaName = "replica1";
+		ReplicaServer replica = new ReplicaServer(replicaName);
+		replica.start();
+	}
+
+}
